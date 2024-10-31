974f85881ddd9ff1095bc80e397a5765665154fe
adam ligocki <a.ligocki@seznam.cz>
2024-10-31 14:49:27 +0100

    added lib2

---
+++ lib2.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+
+namespace second_namespace {
+
+    void another_cool_function() {
+    
+	    std::cout << "Another cool function" << std::endl;
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "lib.hpp"
+#include "lib2.hpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     my_library::my_cool_print_function();
+    second_namespace::another_cool_function();
+
     return 0;
 }
